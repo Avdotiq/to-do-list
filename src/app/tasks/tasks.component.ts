992263bf080ac7a56372814fa7c25a672bbe2bf2
Avdotiq <kbirukova18@gmail.com>
2024-10-09 16:38:23 +0200

    filter for task by assigned user, completed task features

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,51 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name: string | undefined;
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) name: string | undefined;
   @Output() select = new EventEmitter<string>();
 
   onSelectUser() {
     this.select.emit(this.name);
+  }
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u3',
+      title: 'Master Angular',
+      summary: 'Do somrthing',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Master Angular',
+      summary: 'Do somrthing',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Master Angular',
+      summary: 'Do somrthing',
+      dueDate: '2025-12-31',
+    },
+  ]
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id)
   }
 }
